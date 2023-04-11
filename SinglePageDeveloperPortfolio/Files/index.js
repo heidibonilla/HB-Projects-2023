f706107233e37f22f7630b8491e16c05442c7b2d
heidibonilla <heidimbonilla@gmail.com>
2023-04-11 16:48:52 -0400

    projects section completed for desktop

--- SinglePageDeveloperPortfolio/Files/index.js
+++ SinglePageDeveloperPortfolio/Files/index.js
@@ -1,1 +1,10 @@
 // js for contact section
+
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+
+email.addEventListener('input', (event) => {
+ if(email.validity.valid) {
+  
+ }
+})
